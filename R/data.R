964d435d62dd6d27e799c4ccf01ff3ed9d7eb441
ProfJakeVestal <ProfJakeVestal@gmail.com>
2022-01-17 10:36:02 -0500

    namespacing

---
+++ R/data.R
@@ -0,0 +1,11 @@
+#' Registry
+#'
+#' Dataframe containing registered participants by tradername alongside their
+#' status in the onboarding process.
+#'
+#' @format A data frame
+# #' \describe{
+# #'   \item{carat}{weight of the diamond, in carats}
+# #' }
+#'
+"registry"
