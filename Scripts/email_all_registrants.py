404566d4aa0ae717d0da2710a270bb8cc8714c61
ProfJakeVestal <ProfJakeVestal@gmail.com>
2022-01-19 14:39:22 -0500

    update

--- Scripts/email_all_registrants.py
+++ Scripts/email_all_registrants.py
@@ -10,58 +10,31 @@ import os
 import json
 import pandas as pd
 
+
+subject = 'DUKE FINTECH TRADING COMPETITION UPDATE 19 Jan'
+message_filename = 'update_1.txt'
+
 with codecs.open(
-        'C:\\Users\\vcm\\Desktop\\fintech.trading.competition\\Scripts\\Email Templates\\IBKR_is_down.html',
-        'r') as f:
+        'C:\\Users\\vcm\\Desktop\\fintech.trading.competition\\Scripts' + \
+        '\\Email Templates\\' + message_filename, 'r') as f:
     message_body = f.read()
 
-subject = 'DUKE FINTECH TRADING COMPETITION UPDATE 18 Jan'
+invited = pd.read_csv(
+    'C:\\Users\\vcm\\Desktop\\duke_fintech_trading_competition_2022\\' + \
+    'wufoo_registrants.csv'
+).drop_duplicates(subset=['email'])
 
-########################################################################################################################
-# Refresh local WuFoo
+for i, row in invited.iterrows():
 
-def get_wufoo_data(url, un, pwd):
-    req = requests.get(url, auth = (un, pwd))
-    soup = BeautifulSoup(req.text, 'html.parser')
-    return json.loads(soup.text)
-
-def fetch_entries(pgstart):
-    entries_json = get_wufoo_data(os.getenv('WUFOO_ENTRIES_URL').replace("pgstart", str(pgstart)),
-                   os.getenv('WUFOO_UN'), os.getenv('WUFOO_PWD'))['Entries']
-    return pd.json_normalize(entries_json)
-
-number_of_entries_json = get_wufoo_data(os.getenv('WUFOO_COUNT_URL'), os.getenv('WUFOO_UN'), os.getenv('WUFOO_PWD'))
-number_of_entries = int(number_of_entries_json['EntryCount'])
-competition_registrants = fetch_entries(0)
-
-while competition_registrants.shape[0] < number_of_entries:
-    new_fetch = fetch_entries(competition_registrants.shape[0])
-    competition_registrants = pd.concat([competition_registrants, new_fetch])
-
-competition_registrants = competition_registrants.rename(columns={
-  'EntryId': 'id', 'Field1': 'first_name', 'Field2': 'last_name',
-  'Field3': 'email', 'Field4': 'School', 'Field124': 'graduation_year',
-  'Field120': 'undergrad_major', 'Field10': 'graduate_dept', 'Field118': 'sex',
-  'Field116': 'country', 'Field5': 'tradername',  'Field126': 'linkedin'
-})
-
-competition_registrants.to_csv('C:\\Users\\vcm\\Desktop\\duke_fintech_trading_competition_2022\\wufoo_registrants.csv', index = False)
-
-registrants = list(filter(lambda x:'.edu' in x, competition_registrants['email']))
-
-
-for invite_email in registrants:
-
-    print('emailing: ', invite_email)
-
-    participant_row = competition_registrants.loc[competition_registrants['email'] == invite_email]
+    print('emailing: ', row.email)
 
     outlook = win32.Dispatch('outlook.application')
     mail = outlook.CreateItem(0)
-
-    mail.To = invite_email
+    mail.To = row.email
     mail.Subject = subject
-    mail.Body = message_body.replace('firstname', participant_row.iloc[0]['first_name'])
+    mail.Body = message_body.replace(
+        'tradername', row.tradername
+    )
 
     mail.Send()
 
--- data-raw/registry.R
+++ data-raw/registry.R
@@ -2,15 +2,33 @@
 
 library('magrittr')
 
-registry <- readr::read_csv(
+registry <- xml2::read_xml(
   file.path(
     Sys.getenv("APP_BASE_PATH"),
     "duke_fintech_trading_competition_2022",
-    "invited.csv",
+    "EoD_Balances_and_IDs.xml",
     fsep = "\\"
   )
 ) %>%
-  dplyr::inner_join(
+  xml2::xml_child('FlexStatements') %>%
+  xml2::xml_contents() %>%
+  lapply(
+    function(FlexStatement){
+      FlexStatement %>%
+        xml2::xml_child('AccountInformation') %>% {
+          tibble::tibble(
+            'account_id' = xml2::xml_attr(., 'accountId'),
+            'name'       = xml2::xml_attr(., 'name'),
+            'email'      = xml2::xml_attr(., 'primaryEmail')
+          )
+        }
+    }
+  ) %>%
+  purrr::reduce(dplyr::bind_rows) %>%
+  dplyr::nest_by(account_id, name, email) %>%
+  dplyr::select(-data) %>%
+  dplyr::ungroup() %>%
+  dplyr::right_join(
     readr::read_csv(
       file.path(
         Sys.getenv("APP_BASE_PATH"),
@@ -18,10 +36,16 @@ registry <- readr::read_csv(
         "wufoo_registrants.csv",
         fsep = "\\"
       )
-    ),
-    by=c('invited' = 'email')
+    ) %>%
+      dplyr::select(email, tradername),
+    by = 'email'
   ) %>%
-  dplyr::select(tradername) %>%
-  unique()
+  dplyr::filter(!duplicated(email)) %>%
+  dplyr::select(tradername, account_id) %>%
+  dplyr::rename(status = account_id) %>%
+  dplyr::arrange(tradername)
+
+registry$status[!is.na(registry$status)] <- "ACTIVE"
+registry$status[is.na(registry$status)]  <- "pending account creation"
 
 usethis::use_data(registry, overwrite = TRUE)
