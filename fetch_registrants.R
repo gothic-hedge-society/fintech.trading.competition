112c35a644db1fdc89b6f0554642a0614da5faea
JakeVestal <jmvestal@gmail.com>
2023-09-17 12:37:01 -0400

    admin view

--- fetch_registrants.R
+++
@@ -1,6 +0,0 @@
-library(magrittr)
-reticulate::source_python("Scripts/fetch_registrants.py")
-registered_traders <- jsonlite::fromJSON(fetch_registrants())
-
-print(registered_traders)
-View(registered_traders$Entries)
